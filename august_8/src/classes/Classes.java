256270a4ae925d82cb7e8d36a56e3104ac0c2a87
UMAMI <mhmmtuzn61@hotmail.com>
2023-08-08 15:55:38 +0300

    august_8 done

---
+++ august_8/src/classes/Classes.java
@@ -0,0 +1,30 @@
+package classes;
+
+import java.util.Scanner;
+
+public class Classes {
+
+	public static void main(String[] args) {
+		
+		Person p = new Person();
+		
+	}
+	
+	
+
+}
+
+class Person
+{
+	private Integer yas;
+	private String isim;
+	
+	public Person() {
+		System.out.println(Konus("Ben geldim"));
+	}
+	
+	public String Konus(String mesaj)
+	{
+		return "Merhaba " + mesaj;
+	}
+}

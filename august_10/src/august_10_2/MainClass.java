a193dd6a14d090e9a04ac02d0db6a9e7d95c73e0
UMAMI <mhmmtuzn61@hotmail.com>
2023-08-11 11:34:02 +0300

    august_10_challenge done

--- august_10/src/august_10_2/MainClass.java
+++ august_10/src/august_10_2/MainClass.java
@@ -12,6 +12,28 @@ public class MainClass {
 		// ornek2();
 		// ornek3();
 		// ornek7();
+		// replace("numan karaaslan", "la", "-");
+		// System.out.println(Zip("numan karaaslan"));
+		// challenge1("numan karaaslan");
+		challenge2("numan karaaslan", "n", "xxx");
+	}
+
+	private static void challenge2(String text, String from, String to) {
+
+	}
+
+	private static void challenge1(String text) {
+
+		String[] temp = text.split(" ");
+
+		for (int i = 0; i < temp.length; i++) {
+
+			for (int j = 0; j < temp[i].length() - 2; j++) {
+
+				System.out.print(temp[i].charAt(j));
+			}
+			System.out.print("-- ");
+		}
 
 	}
 
@@ -27,22 +49,97 @@ public class MainClass {
 	// numan karaaslan
 	// xxxumaxxx- karaaslaxxx
 
-	private static String replace(String text, String from, String to) {
-		String newOutput = "";
-		
+	private static String Zip(String text) {
+		String temp = "";
+
 		for (int i = 0; i < text.length(); i++) {
-			
-			if(text.charAt(i) == from.charAt(i))
-			{
-				for(int j =0; j<from.length();j++)
-				{
-					if(text.charAt(i+j) == from.charAt(j))
+			if (CheckIfExist(text, i)) {
+				continue;
+			}
+			temp += text.charAt(i);
+		}
+
+		return temp;
+	}
+
+	private static boolean CheckIfExist(String text, int index) {
+		for (int i = 0; i < index; i++) {
+			if (text.charAt(i) == text.charAt(index))
+				return true;
+		}
+		return false;
+	}
+
+	private static void replace(String text, String from, String to) {
+		String newOutput = "";
+		int i = 0;
+		// tüm textin indis sayısı kadar döngü oluşturuyoruz
+		for (; i < text.length(); i++) {
+			// eğer değiştireceğiiz kelimenin ilk harfini bulduysak sorguya giriyoruz
+			// tüm texti değiştireceğimiz kelimenin ilk harfini bulana kadar döndürüyoruz
+			if (text.charAt(i) == from.charAt(0)) {
+				// eğer ilk harf ve sonrasındaki tüm harfler uyuşuyorsa eşitlik değişkenini
+				// ayarlıyoruz
+				// döngüde j yi i nin kaldığı yerden başlatıyoruz. i nin başladığı yerden
+				// değiştireceğimiz kelimenin uzunluğu kadar döngüyü sürdüreceğiz
+				// mesela değiştireceğimiz kelimenin ilk harfini 3. indiste bulduysak 3. indisi
+				// j y everiyoruz
+				// 3. indisin yanında aradığımız kelimenin ilk harfine karşılık geldiğinden 0
+				// dan başlayan ayrı bir değişkenle ikisinin de eşitliğini sorguluyoruz
+				if (CheckIsEqual(text, from, i)) {
+					newOutput += to;
+					i += from.length();
+					System.out.println(from.length() + " " + i + " " + text.length());
+				}
+				System.out.println(from.length() + " " + i + " " + text.length());
+			}
+			newOutput += text.charAt(i);
+		}
+		System.out.println(newOutput);
+	}
+
+	private static boolean CheckIsEqual(String text, String from, int index) {
+		int j = index;
+		int k = 0;
+		for (; j < from.length() + index; j++) {
+			if (text.charAt(j) != from.charAt(k++)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static void replaceChallenge()
+
+	{
+
+		String replaced = " n";
+		String replacer = " n n ";
+		String deger = "n numan nkaraaslan n";
+		String result = "";
+
+		for (int i = 0; i < deger.length(); i++) {
+
+			boolean exists = true;
+
+			for (int j = 0; j < replaced.length(); j++) {
+				if (i + j == deger.length()) {
+					exists = false;
+					break;
+				}
+				if (deger.charAt(i + j) != replaced.charAt(j)) {
+					exists = false;
+					break;
 				}
 			}
-			else
-				newOutput +=text.charAt(i);
+			if (exists) {
+				result += replacer;
+				i += replaced.length() - 1;
+			} else {
+				result += deger.charAt(i);
+			}
 		}
-		return newOutput;
+		System.out.print(result);
 	}
 
 	private static void ornek7() {
---
+++ august_10/src/august_10_2/Odev1.java
@@ -0,0 +1,99 @@
+package august_10_2;
+
+public class Odev1 {
+
+	public static void main(String[] args) {
+		replace("numan karaaslan ", "a", "--");
+		// System.out.println(Zip("numan karaaslan"));
+		// challenge1("numan karaaslan");
+
+	}
+
+	private static void challenge1(String text) {
+
+		String[] temp = text.split(" ");
+
+		for (int i = 0; i < temp.length; i++) {
+
+			for (int j = 0; j < temp[i].length() - 2; j++) {
+
+				System.out.print(temp[i].charAt(j));
+			}
+			System.out.print("-- ");
+		}
+
+	}
+
+	// zip challenge
+	// numankaraaslan -> numakrsl
+
+	// numan karaaslan
+	// num-- karaasl--
+
+	// numan karaaslan
+	// num- karaasl-
+
+	// numan karaaslan
+	// xxxumaxxx- karaaslaxxx
+
+	private static String Zip(String text) {
+		String temp = "";
+
+		for (int i = 0; i < text.length(); i++) {
+			if (CheckIfExist(text, i)) {
+				continue;
+			}
+			temp += text.charAt(i);
+		}
+
+		return temp;
+	}
+
+	private static boolean CheckIfExist(String text, int index) {
+		for (int i = 0; i < index; i++) {
+			if (text.charAt(i) == text.charAt(index))
+				return true;
+		}
+		return false;
+	}
+
+	private static void replace(String text, String from, String to) {
+		String newOutput = "";
+		int i = 0;
+		// tüm textin indis sayısı kadar döngü oluşturuyoruz
+		for (; i < text.length(); i++) {
+			// eğer değiştireceğiiz kelimenin ilk harfini bulduysak sorguya giriyoruz
+			// tüm texti değiştireceğimiz kelimenin ilk harfini bulana kadar döndürüyoruz
+			if (text.charAt(i) == from.charAt(0)) {
+				// eğer ilk harf ve sonrasındaki tüm harfler uyuşuyorsa eşitlik değişkenini
+				// ayarlıyoruz
+				// döngüde j yi i nin kaldığı yerden başlatıyoruz. i nin başladığı yerden
+				// değiştireceğimiz kelimenin uzunluğu kadar döngüyü sürdüreceğiz
+				// mesela değiştireceğimiz kelimenin ilk harfini 3. indiste bulduysak 3. indisi
+				// j y everiyoruz
+				// 3. indisin yanında aradığımız kelimenin ilk harfine karşılık geldiğinden 0
+				// dan başlayan ayrı bir değişkenle ikisinin de eşitliğini sorguluyoruz
+				if (CheckIsEqual(text, from, i)) {
+					newOutput += to;
+					i += from.length();
+					// System.out.println(from.length() + " " + i + " " + text.length());
+				}
+				// System.out.println(from.length() + " " + i + " " + text.length());
+			}
+			newOutput += text.charAt(i);
+		}
+		System.out.println(newOutput);
+	}
+
+	private static boolean CheckIsEqual(String text, String from, int index) {
+		int j = index;
+		int k = 0;
+		for (; j < from.length() + index; j++) {
+			if (text.charAt(j) != from.charAt(k++)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
